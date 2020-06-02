cee130e7e37bcd34b479405567817acd809746f4
John Twigg <john.twigg@creditkarma.com>
2020-06-02 16:59:53 -0700

    Remove comment

--- Sources/AutoWiring.swift
+++ Sources/AutoWiring.swift
@@ -65,8 +65,6 @@ extension DependencyContainer {
    
     var definitions = self.definitions[type: key.type].map({ (key: $0.0, definition: $0.1) })
     
-    //var definitions = self.definitions.map({ (key: $0.0, definition: $0.1) })
-    
     definitions = filter(definitions: definitions, byKey: key, strictByTag: strictByTag)
     definitions = definitions.sorted(by: { $0.definition.numberOfArguments > $1.definition.numberOfArguments })
     
