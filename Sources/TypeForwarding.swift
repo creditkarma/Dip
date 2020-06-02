d9fb0d8cb8025c001b1ad92fca75e1702a2e301e
John Twigg <john.twigg@creditkarma.com>
2020-06-02 10:36:06 -0700

    Speed up type forwarding.

--- Sources/TypeForwarding.swift
+++ Sources/TypeForwarding.swift
@@ -131,7 +131,9 @@ extension DependencyContainer {
   
   /// Searches for definition that forwards requested type
   func typeForwardingDefinition(forKey key: DefinitionKey) -> KeyDefinitionPair? {
-    var forwardingDefinitions = self.definitions.map({ (key: $0.0, definition: $0.1) })
+    var forwardingDefinitions = self.definitionsByType[ObjectIdentifier(key.type).hashValue]?.map({ (key: $0.0, definition: $0.1) }) ?? []
+    
+    //var forwardingDefinitions = self.definitionsByType.map({ (key: $0.0, definition: $0.1) })
     
     forwardingDefinitions = filter(definitions: forwardingDefinitions, byKey: key, byTypeOfArguments: true)
     forwardingDefinitions = order(definitions: forwardingDefinitions, byTag: key.tag)
