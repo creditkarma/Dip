bfe2cb8b5234591e71aa6719bc2fb9269263595c
John Twigg <john.twigg@creditkarma.com>
2020-06-04 10:42:41 -0700

    PR Comment

--- Sources/Dip.swift
+++ Sources/Dip.swift
@@ -582,7 +582,7 @@ extension DependencyContainer.Tag: Equatable {
 final class DefinitionsContainer {
   // Backing data structure. Its a map of
   // [Type.hashValue: [DefinitionKey: _Definition]]
-  // so we have fash lookup of by Type for the definitions that handle it.
+  // so we have fast lookup of by Type for the definitions that handle it.
   private var definitionsByType = [Int: [DefinitionKey: _Definition]]()
   
   typealias DefinitionKeyValuePair = (DefinitionKey, _Definition)
